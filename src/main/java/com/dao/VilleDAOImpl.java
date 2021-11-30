04f3cc67c5b5f213e2fbf07075aac696ef4ca5aa
LEBLED Arthur <arthur.lebled@reseau.eseo.fr>
2021-11-30 16:27:45 +0100

    Correction sonar

--- src/main/java/com/dao/VilleDAOImpl.java
+++ src/main/java/com/dao/VilleDAOImpl.java
@@ -10,28 +10,37 @@ import java.util.List;
 
 @Repository
 public class VilleDAOImpl implements VilleDAO {
+    private String codeCommuneKey = "Code_commune_INSEE";
+    private String nomCommuneKey = "Nom_commune";
+    private String ligne5Key = "Ligne_5";
+    private String codePostalKey = "Code_postal";
+    private String libelleKey = "Libelle_acheminement";
+    private String latitudeKey = "Latitude";
+    private String longitudeKey = "Longitude";
+
     @Override
     public List<Ville> getListeVilles() {
-        Statement statement = null;
         ResultSet resultat = null;
         List<Ville> listVilles = new ArrayList<>();
+
+
 
         Connection connexion;
         try {
             connexion = JDBCConfiguration.getConnection();
 
-            try {
-                statement = connexion.createStatement();
+            try(Statement statement = connexion.createStatement()) {
+
                 resultat = statement.executeQuery("SELECT * FROM ville_france;");
                 while (resultat.next()) {
                     Ville v = new Ville(
-                            resultat.getString("Code_commune_INSEE"),
-                            resultat.getString("Nom_commune"),
-                            resultat.getString("Code_postal"),
-                            resultat.getString("Libelle_acheminement"),
-                            resultat.getString("Ligne_5"),
-                            resultat.getString("Latitude"),
-                            resultat.getString("Longitude")
+                            resultat.getString(codeCommuneKey),
+                            resultat.getString(nomCommuneKey),
+                            resultat.getString(codePostalKey),
+                            resultat.getString(libelleKey),
+                            resultat.getString(ligne5Key),
+                            resultat.getString(latitudeKey),
+                            resultat.getString(longitudeKey)
                     );
                     listVilles.add(v);
                 }
@@ -48,16 +57,12 @@ public class VilleDAOImpl implements VilleDAO {
 
     @Override
     public List<String> getNomsVilles() {
-        Statement statement = null;
         ResultSet resultat = null;
         List<String> nom = new ArrayList<>();
-
         Connection connexion;
         try {
             connexion = JDBCConfiguration.getConnection();
-
-            try {
-                statement = connexion.createStatement();
+            try(Statement statement = connexion.createStatement()) {
                 resultat = statement.executeQuery("SELECT Nom_commune FROM ville_france;");
                 while (resultat.next()) {
                     nom.add(resultat.getString("Nom_commune"));
@@ -77,26 +82,24 @@ public class VilleDAOImpl implements VilleDAO {
     public Ville trouverVille(Ville ville) {
         Connection connexion;
         Ville resultVille = null;
-        Statement statement = null;
         ResultSet resultat = null;
         try {
             connexion = JDBCConfiguration.getConnection();
 
-            try {
+            try(Statement statement = connexion.createStatement()) {
 
-                statement = connexion.createStatement();
                 resultat = statement.executeQuery(
                         "SELECT * FROM ville_france WHERE Code_commune_INSEE =" + ville.getCodeCommuneINSEE());
 
                 if(resultat.next()) {
                     ville = new Ville(
-                            resultat.getString("Code_commune_INSEE"),
-                            resultat.getString("Nom_Commune"),
-                            resultat.getString("Code_postal"),
-                            resultat.getString("Libelle_acheminement"),
-                            resultat.getString("Ligne_5"),
-                            resultat.getString("Latitude"),
-                            resultat.getString("Longitude")
+                            resultat.getString(codeCommuneKey),
+                            resultat.getString(nomCommuneKey),
+                            resultat.getString(codePostalKey),
+                            resultat.getString(libelleKey),
+                            resultat.getString(ligne5Key),
+                            resultat.getString(latitudeKey),
+                            resultat.getString(longitudeKey)
                     );
 
                     resultVille = ville;
@@ -116,7 +119,6 @@ public class VilleDAOImpl implements VilleDAO {
     public List<Ville> trouverVilleParCodePostal(String codePostal) {
         Connection connexion;
         Ville ville = null;
-        Statement statement = null;
         ResultSet resultat = null;
 
         List<Ville> villes = new ArrayList<>();
@@ -124,20 +126,19 @@ public class VilleDAOImpl implements VilleDAO {
         try {
             connexion = JDBCConfiguration.getConnection();
 
-            try {
+            try(Statement statement = connexion.createStatement()) {
 
-                statement = connexion.createStatement();
                 resultat = statement.executeQuery("SELECT * FROM ville_france where Code_Postal =" + codePostal);
 
                 while (resultat.next()) {
                     ville = new Ville(
-                            resultat.getString("Code_commune_INSEE"),
-                            resultat.getString("Nom_Commune"),
-                            resultat.getString("Code_postal"),
-                            resultat.getString("Libelle_acheminement"),
-                            resultat.getString("Ligne_5"),
-                            resultat.getString("Latitude"),
-                            resultat.getString("Longitude")
+                            resultat.getString(codeCommuneKey),
+                            resultat.getString(nomCommuneKey),
+                            resultat.getString(codePostalKey),
+                            resultat.getString(libelleKey),
+                            resultat.getString(ligne5Key),
+                            resultat.getString(latitudeKey),
+                            resultat.getString(longitudeKey)
                     );
                     villes.add(ville);
                 }
@@ -156,26 +157,24 @@ public class VilleDAOImpl implements VilleDAO {
     public Ville trouverVilleParNomCommune(String nomCommune) {
         Connection connexion;
         Ville ville = null;
-        Statement statement = null;
         ResultSet resultat = null;
 
         try {
             connexion = JDBCConfiguration.getConnection();
 
-            try {
+            try(Statement statement = connexion.createStatement()) {
 
-                statement = connexion.createStatement();
                 resultat = statement.executeQuery("SELECT * FROM ville_france where Nom_commune ='" + nomCommune+"'");
 
                 while (resultat.next()) {
                     ville = new Ville(
-                            resultat.getString("Code_commune_INSEE"),
-                            resultat.getString("Nom_Commune"),
-                            resultat.getString("Code_postal"),
-                            resultat.getString("Libelle_acheminement"),
-                            resultat.getString("Ligne_5"),
-                            resultat.getString("Latitude"),
-                            resultat.getString("Longitude")
+                            resultat.getString(codeCommuneKey),
+                            resultat.getString(nomCommuneKey),
+                            resultat.getString(codePostalKey),
+                            resultat.getString(libelleKey),
+                            resultat.getString(ligne5Key),
+                            resultat.getString(latitudeKey),
+                            resultat.getString(longitudeKey)
                     );
                 }
 
@@ -195,9 +194,8 @@ public class VilleDAOImpl implements VilleDAO {
         try {
             connexion = JDBCConfiguration.getConnection();
 
-            try {
-                PreparedStatement preparedStatement = connexion
-                        .prepareStatement("INSERT INTO ville_france VALUES(?,?,?,?,?,?,?)");
+            try(PreparedStatement preparedStatement = connexion
+                    .prepareStatement("INSERT INTO ville_france VALUES(?,?,?,?,?,?,?)")) {
                 preparedStatement.setString(1, ville.getCodeCommuneINSEE());
                 preparedStatement.setString(2, ville.getNomCommune());
                 preparedStatement.setString(3, ville.getCodePostal());
@@ -207,8 +205,6 @@ public class VilleDAOImpl implements VilleDAO {
                 preparedStatement.setString(7, ville.getLongitude());
                 try {
                     preparedStatement.executeUpdate();
-                    System.out.println("execute");
-                    preparedStatement.close();
                 } catch (SQLException e) {
                     System.err.println(e.getLocalizedMessage());
                 }
@@ -226,14 +222,12 @@ public class VilleDAOImpl implements VilleDAO {
         Connection connexion;
         try {
             connexion = JDBCConfiguration.getConnection();
-            try {
-                PreparedStatement preparedStatement = connexion
-                        .prepareStatement("DELETE FROM ville_france WHERE code_commune_INSEE = ?");
+            try(PreparedStatement preparedStatement = connexion
+                    .prepareStatement("DELETE FROM ville_france WHERE code_commune_INSEE = ?")) {
+
                 preparedStatement.setString(1, codeInsee);
                 try {
                     preparedStatement.executeUpdate();
-                    System.out.println("execute");
-                    preparedStatement.close();
                 } catch (SQLException e) {
                     System.err.println(e.getLocalizedMessage());
                 }
@@ -252,10 +246,9 @@ public class VilleDAOImpl implements VilleDAO {
         try {
             connexion = JDBCConfiguration.getConnection();
 
-            try {
-                PreparedStatement preparedStatement = connexion.prepareStatement(
-                        "UPDATE ville_france SET nom_commune = ?, code_postal = ?, libelle_acheminement = ?," +
-                                " ligne_5 = ?, latitude = ?, longitude= ? WHERE code_commune_INSEE = ?");
+            try(PreparedStatement preparedStatement = connexion.prepareStatement(
+                    "UPDATE ville_france SET nom_commune = ?, code_postal = ?, libelle_acheminement = ?," +
+                            " ligne_5 = ?, latitude = ?, longitude= ? WHERE code_commune_INSEE = ?")) {
                 preparedStatement.setString(1, ville.getNomCommune());
                 preparedStatement.setString(2, ville.getCodePostal());
                 preparedStatement.setString(3, ville.getLibelleAcheminement());
@@ -266,7 +259,6 @@ public class VilleDAOImpl implements VilleDAO {
                 try {
                     preparedStatement.executeUpdate();
                     System.out.println("execute");
-                    preparedStatement.close();
                 } catch (SQLException e) {
                     System.err.println(e.getLocalizedMessage());
                 }
