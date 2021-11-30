f53ee649504b00c03b74eb7a324fae11484e683c
LEBLED Arthur <arthur.lebled@reseau.eseo.fr>
2021-11-30 16:02:46 +0100

    Correction sonar

--- src/main/java/com/config/JDBCConfiguration.java
+++ src/main/java/com/config/JDBCConfiguration.java
@@ -3,33 +3,36 @@ package com.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.dao.DaoException;
 
+
 @Configuration
 public class JDBCConfiguration {
+
+    @Value("${database.user}")
+    private static String USERNAME;
+    @Value("${database.password}")
+    private static String PASSWORD;
 
     @Bean
     public static Connection getConnection() throws DaoException {
         String connectionURL = "jdbc:mysql://localhost:3306/ProjetTWIC?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
-        String user = "root";
-        String pwd = "";
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e2) {
-            throw new DaoException("Impossible de se connecter à la base de données");
-        }
-
-        Connection connexion = null;
-        try {
-            connexion = DriverManager.getConnection(connectionURL,user,pwd);
+        try (Connection connexion = DriverManager.getConnection(connectionURL,USERNAME,PASSWORD)){
             connexion.setAutoCommit(false);
+            return connexion;
         } catch (SQLException e1) {
             throw new DaoException("Impossible de se connecter à la base de données " + e1.getLocalizedMessage());
         }
 
-        return connexion;
+    }
+
+    @Value("${database.user}")
+    public synchronized void setUserP(String user){
+        JDBCConfiguration.USERNAME = user;
     }
 }
